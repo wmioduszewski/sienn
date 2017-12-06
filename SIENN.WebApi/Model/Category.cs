fd03cc27ac845211287f74994f781165578bca3b
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-12-06 18:09:21 +0000

    Added initial models

---
+++ SIENN.WebApi/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIENN.WebApi.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ SIENN.WebApi/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace SIENN.WebApi.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        public Type Type { get; set; }
+        public Unit Unit { get; set; }
+
+        public Product()
+        {
+            Categories = new Collection<Category>();
+        }
+    }
+}
