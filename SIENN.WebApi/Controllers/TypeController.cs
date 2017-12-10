3ca59885a8226a54d5608c2644665f67430c4f15
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-12-10 22:48:08 +0000

    First API method on Type

---
+++ SIENN.WebApi/Controllers/TypeController.cs
@@ -0,0 +1,26 @@
+﻿namespace SIENN.WebApi.Controllers
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DbAccess.Persistance;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Services.Model;
+
+    public class TypeController : Controller
+    {
+        private readonly SiennDbContext context;
+
+        public TypeController(SiennDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("api/types")]
+        public IEnumerable<Type> GetTypes()
+        {
+            return context.Type.ToList();
+        }
+    }
+}
