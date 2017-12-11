678da22c3e4ff163804a3096eeaccfed7d30f818
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-12-11 01:30:05 +0000

    First two simple controllers

--- SIENN.WebApi/Controllers/TypeController.cs
+++ SIENN.WebApi/Controllers/TypeController.cs
@@ -3,24 +3,43 @@
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using AutoMapper;
     using DbAccess.Persistance;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Services.Model;
+    using Services.Resources;
 
+    [Route("api/types")]
     public class TypeController : Controller
     {
         private readonly SiennDbContext context;
+        private readonly IMapper mapper;
 
-        public TypeController(SiennDbContext context)
+        public TypeController(SiennDbContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
-        [HttpGet("api/types")]
-        public IEnumerable<Type> GetTypes()
+        [HttpGet]
+        public IEnumerable<TypeResource> GetTypes()
         {
-            return context.Type.ToList();
+            var types = context.Type.ToList();
+            return mapper.Map<List<Type>, List<TypeResource>>(types);
+        }
+
+        [HttpPost]
+        public IActionResult CreateType([FromBody] TypeResource typeResource)
+        {
+            var type = mapper.Map<TypeResource, Type>(typeResource);
+
+            context.Type.Add(type);
+            context.SaveChanges();
+
+            var result = mapper.Map<Type, TypeResource>(type);
+
+            return Ok(result);
         }
     }
 }
---
+++ SIENN.WebApi/Controllers/UnitController.cs
@@ -0,0 +1,43 @@
+﻿namespace SIENN.WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using DbAccess.Persistance;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Model;
+    using Services.Resources;
+
+    [Route("api/units")]
+    public class UnitController : Controller
+    {
+        private readonly SiennDbContext context;
+        private readonly IMapper mapper;
+
+        public UnitController(SiennDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<UnitResource> GetTypes()
+        {
+            var units = context.Unit.ToList();
+            return mapper.Map<List<Unit>, List<UnitResource>>(units);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUnit([FromBody] UnitResource unitResource)
+        {
+            var unit = mapper.Map<UnitResource, Unit>(unitResource);
+
+            context.Unit.Add(unit);
+            context.SaveChanges();
+
+            var result = mapper.Map<Unit, UnitResource>(unit);
+
+            return Ok(result);
+        }
+    }
+}
