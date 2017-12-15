bb06053d1144740b5bbdc56841fd91fb2eb30c9e
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-12-15 23:41:40 +0000

    Added mapping for Product, added Product controller

--- SIENN.Services/Mapping/MappingProfile.cs
+++ SIENN.Services/Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿namespace SIENN.Services.Mapping
 {
+    using System.Linq;
     using AutoMapper;
     using Model;
     using Resources;
@@ -12,14 +13,34 @@
             CreateMap<Type, TypeResource>();
             CreateMap<Unit, UnitResource>();
             CreateMap<Category, CategoryResource>();
-            CreateMap<Product, ProductResource>();
+
+            CreateMap<Product, ProductResource>()
+                .ForMember(pr => pr.Categories, opt => opt.MapFrom(p => p.Categories.Select(pc => pc.CategoryId)));
 
             //API to domain
             CreateMap<TypeResource, Type>();
             CreateMap<UnitResource, Unit>();
-
             CreateMap<CategoryResource, Category>()
                 .ForMember(x=>x.Products, opt => opt.Ignore());
+
+            CreateMap<ProductResource, Product>()
+                .ForMember(p => p.Categories, opt => opt.Ignore())
+                .AfterMap((pr, p) =>
+                {
+                    //remove unselected categories
+                    var removedCategories = p.Categories.Where(c => !pr.Categories.Contains(c.CategoryId)).ToList();
+                    foreach (var category in removedCategories)
+                    {
+                        p.Categories.Remove(new CategoryToProduct(){CategoryId = category.CategoryId,ProductId = p.Id});
+                    }
+
+                    var addedCategories = pr.Categories.Where(id => p.Categories.All(c => c.CategoryId != id))
+                        .Select(id => new CategoryToProduct(){CategoryId = id,ProductId = pr.Id}).ToList();
+                    foreach (var categoryToProduct in addedCategories)
+                    {
+                        p.Categories.Add(categoryToProduct);
+                    }
+                });
         }
     }
 }
---
+++ SIENN.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SIENN.WebApi.Controllers
+{
+    using System.Data.SqlClient;
+    using AutoMapper;
+    using DbAccess.Persistance;
+    using Microsoft.EntityFrameworkCore;
+    using Services.Model;
+    using Services.Resources;
+
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        private readonly SiennDbContext context;
+        private readonly IMapper mapper;
+
+        public ProductsController(SiennDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductResource> GetProducts()
+        {
+            var products = context.Products.Include(p=>p.Categories).ToList();
+            return mapper.Map<List<Product>, List<ProductResource>>(products);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] ProductResource productResource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var product = mapper.Map<ProductResource, Product>(productResource);
+
+            try
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                return NotFound("Unit, type or category couldn't be found\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            var result = mapper.Map<Product, ProductResource>(product);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(product);
+            context.SaveChanges();
+
+            return Ok(id);
+        }
+    }
+}
